aafcdadc7230e3f5119d68c41acd3b517bd96e9e
shikiomi <rykaperez.z@gmail.com>
2025-08-01 12:57:02 +0800

    fix: Adjust margin in Footer component for improved alignment of copyright text

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400">
         <div className="pt-4 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center">
-          <p className="text-sm ml-5 mb-3">&copy; 2025 Kweynnie. All rights reserved.</p>
+          <p className="text-sm ml-9 mb-3">&copy; 2025 Kweynnie. All rights reserved.</p>
         </div>
     </footer>
   );
